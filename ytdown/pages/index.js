8102bf1e571d171cc4023bf069b01db815e8c28a
shivam agarwal <shivamagarwaloff@gmail.com>
2022-05-05 00:47:17 +0530

    Update index.js

--- ytdown/pages/index.js
+++ ytdown/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
       </div> 
       <div>
         <label className={styles.search}>Search Here, </label>
-        <form action='http://localhost:3000/download/' method="GET">
+        <form action='http://localhost:4000/download/' method="GET">
         <input className={styles.inputBox} type="text"  onChange={inputvalue} name="url"  placeholder='https://www.youtube.com/watch?v=B31LgI4Y4DQ&t=1213s'></input>
         <input className={styles.submit} value="Download" type="submit"></input>
         </form>
